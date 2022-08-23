ff0abc8cfd816373a41cbae84d703991af114359
Николай <Kolya_popov_86@tut.by>
2022-08-23 20:44:40 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N. 5 -> 2, 4
+
+int Prompt(string message)
+{
+    System.Console.Write(message);         // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue);     // Преобразует строку в целое число
+    return result;                         // Возвращает результат 
+}
+
+int N;
+N = Prompt("Введите число N >");
+for (int i = 1; i <= N; i++)
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
